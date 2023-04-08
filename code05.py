3e41e8fb7261cb2e2562abaca7982d778a941a71
Haorui Yang <83082501+Jasper-Rui@users.noreply.github.com>
2023-04-07 22:29:32 -0400

    update

---
+++ code05.py
@@ -0,0 +1,6 @@
+a = 10
+print(type(a))
+
+
+b = 0.5
+print(type(b))
---
+++ code06.py
@@ -0,0 +1,23 @@
+a = 'string'
+
+b = "string"
+
+
+print(type(a))
+
+print(type(b))
+
+print(a == b)
+
+
+c = 'my name is "Jasper"'
+
+d = "my name is 'Jasper'"
+
+print(c)
+print(d)
+print(c == d)
+
+e = '''my name is 'Jasper' and my name is "Jasper"'''
+
+print(e)
