276c33c9f6fa9c239f675738db5ea390d3edd793
Haorui Yang <83082501+Jasper-Rui@users.noreply.github.com>
2023-04-23 00:41:49 -0400

    Update code20.py

---
+++ code20.py
@@ -0,0 +1,20 @@
+for i in range(1, 11):
+    print(i)
+
+
+print()
+
+for i in range(0, 11, 2):
+    print(i)
+
+
+print()
+
+for i in range(10, 1, -1):
+    print(i)
+
+the_sum = 0
+for i in range(1, 101):
+    the_sum += i
+
+print(the_sum)
